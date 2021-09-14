4ab1235ecc340d23fc5787c7da9cc545fdc93aec
Auctioneer99 <42092940+Auctioneer99@users.noreply.github.com>
2021-09-14 23:37:35 +0500

    ignore

--- DiscordBotRecognition/Credentials/Credential.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DiscordBotRecognition.Credentials
-{
-    public static class Credential
-    {
-        public static string DiscordToken => "ODg1MDk0NTI5MTE4NjU0NDc0.YTiChA._IeXFJLevXi7sLCqT5Cmy_M_KRg";
-        public static string GoogleAPIToken => "AIzaSyBkZP11D3Qi02OAHTnFYvB8G32F2e247h8";
-    }
-}
